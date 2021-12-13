eddc575b0ab5e0eb0f0c79162cc3ff0f4b068ee2
disys-akash <95430410+disys-akash@users.noreply.github.com>
2021-12-12 21:34:45 -0800

    Add files via upload

---
+++ phone tracking.py
@@ -0,0 +1,181 @@
+class phonetracking:
+    def __init__(self):
+        self.Watsapp   = {"screen_on_time":"27mins", "notification_recived":"135 notifications","times_opened":"50times"}
+        self.Insatgram ={"screen_on_time":"2hr_27mins","notification_recived":"24 notifications","times_opened":"60times"}
+        self.Snapchat  = {"screen_on_time":"21mins","notification_recived":"71 notifications","times_opened":"13times"}
+        self.Phone     ={"screen_on_time":"1hr_14mins","notification_recived":"60 notifications","times_opened":"59times"}
+        self.Youtube   ={"screen_on_time":"1hr_7mins","notification_recived":"5 notifications","times_opened":"9times"}
+        print("Time used  in a day \n")
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Snapchat --",self.Snapchat["screen_on_time"])
+        print("Insatgram --",self.Insatgram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Snapchat --",self.Snapchat["notification_recived"])
+        print("Insatgram --",self.Insatgram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Snapchat --",self.Snapchat["times_opened"])
+        print("Insatgram --",self.Insatgram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+    def monday(self):
+        self.Watsapp   = {"screen_on_time":"32mins", "notification_recived":"214 notifications","times_opened":"48times"}
+        self.Insatgram ={"screen_on_time":"58mins","notification_recived":"14 notifications","times_opened":"22times"}
+        self.Snapchat  = {"screen_on_time":"9mins","notification_recived":"57 notifications","times_opened":"14times"}
+        self.Phone     ={"screen_on_time":"14mins","notification_recived":"30 notifications","times_opened":"32times"}
+        self.Youtube   ={"screen_on_time":"1hr_3mins","notification_recived":"5 notifications","times_opened":"7times"}
+        print("Time used  in a day \n")
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Snapchat --",self.Snapchat["screen_on_time"])
+        print("Insatgram --",self.Insatgram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Snapchat --",self.Snapchat["notification_recived"])
+        print("Insatgram --",self.Insatgram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Snapchat --",self.Snapchat["times_opened"])
+        print("Insatgram --",self.Insatgram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+    def tuesday(self):
+        self.Watsapp   = {"screen_on_time":"27mins", "notification_recived":"169 notifications","times_opened":"57times"}
+        self.Insatgram ={"screen_on_time":"58mins","notification_recived":"18 notifications","times_opened":"25times"}
+        self.Snapchat  = {"screen_on_time":"9mins","notification_recived":"43 notifications","times_opened":"13times"}
+        self.Phone     ={"screen_on_time":"10mins","notification_recived":"27 notifications","times_opened":"44times"}
+        self.Youtube   ={"screen_on_time":"16mins","notification_recived":"5 notifications","times_opened":"6times"}
+        print("Time used  in a day \n")
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Snapchat --",self.Snapchat["screen_on_time"])
+        print("Insatgram --",self.Insatgram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Snapchat --",self.Snapchat["notification_recived"])
+        print("Insatgram --",self.Insatgram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Snapchat --",self.Snapchat["times_opened"])
+        print("Insatgram --",self.Insatgram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+    def wednesday(self):
+        self.Watsapp   = {"screen_on_time":"27mins", "notification_recived":"155 notifications","times_opened":"80times"}
+        self.Insatgram ={"screen_on_time":"55mins","notification_recived":"16 notifications","times_opened":"55times"}
+        self.Snapchat  = {"screen_on_time":"42mins","notification_recived":"23 notifications","times_opened":"56times"}
+        self.Phone     ={"screen_on_time":"1hr_30mins","notification_recived":"74 notifications","times_opened":"22times"}
+        self.Youtube   ={"screen_on_time":"55mins","notification_recived":"5 notifications","times_opened":"3times"}
+        print("Time used  in a day \n")
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Snapchat --",self.Snapchat["screen_on_time"])
+        print("Insatgram --",self.Insatgram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Snapchat --",self.Snapchat["notification_recived"])
+        print("Insatgram --",self.Insatgram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Snapchat --",self.Snapchat["times_opened"])
+        print("Insatgram --",self.Insatgram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+    def thursday(self):
+        self.Watsapp   = {"screen_on_time":"60mins", "notification_recived":"122 notifications","times_opened":"48times"}
+        self.Insatgram ={"screen_on_time":"1hr_17mins","notification_recived":"37 notifications","times_opened":"61times"}
+        self.Snapchat  = {"screen_on_time":"39mins","notification_recived":"96 notifications","times_opened":"19times"}
+        self.Phone     ={"screen_on_time":"1hr_34mins","notification_recived":"66 notifications","times_opened":"74times"}
+        self.Youtube   ={"screen_on_time":"57mins","notification_recived":"5 notifications","times_opened":"22times"}
+        print("Time used  in a day \n")
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Snapchat --",self.Snapchat["screen_on_time"])
+        print("Insatgram --",self.Insatgram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Snapchat --",self.Snapchat["notification_recived"])
+        print("Insatgram --",self.Insatgram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Snapchat --",self.Snapchat["times_opened"])
+        print("Insatgram --",self.Insatgram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+    def friday(self):
+        self.Watsapp   = {"screen_on_time":"45mins", "notification_recived":"177 notifications","times_opened":"85times"}
+        self.Insatgram ={"screen_on_time":"1hr_18mins","notification_recived":"65 notifications","times_opened":"44times"}
+        self.Snapchat  = {"screen_on_time":"22mins","notification_recived":"91 notifications","times_opened":"29times"}
+        self.Phone     ={"screen_on_time":"1hr_18mins","notification_recived":"69 notifications","times_opened":"89times"}
+        self.Youtube   ={"screen_on_time":"1hr_57mins","notification_recived":"5 notifications","times_opened":"35times"}
+        print("Time used  in a day \n")
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Snapchat --",self.Snapchat["screen_on_time"])
+        print("Insatgram --",self.Insatgram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Snapchat --",self.Snapchat["notification_recived"])
+        print("Insatgram --",self.Insatgram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Snapchat --",self.Snapchat["times_opened"])
+        print("Insatgram --",self.Insatgram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+    def saturday(self):
+        self.Watsapp   = {"screen_on_time":"27mins", "notification_recived":"135 notifications","times_opened":"50times"}
+        self.Insatgram ={"screen_on_time":"2hr_27mins","notification_recived":"24 notifications","times_opened":"60times"}
+        self.Snapchat  = {"screen_on_time":"21mins","notification_recived":"71 notifications","times_opened":"13times"}
+        self.Phone     ={"screen_on_time":"1hr_14mins","notification_recived":"60 notifications","times_opened":"59times"}
+        self.Youtube   ={"screen_on_time":"1hr_7mins","notification_recived":"5 notifications","times_opened":"9times"}
+        print("Time used  in a day \n")
+        print("Watsapp --",self.Watsapp["screen_on_time"])
+        print("Snapchat --",self.Snapchat["screen_on_time"])
+        print("Insatgram --",self.Insatgram["screen_on_time"])
+        print("Youtube --",self.Youtube["screen_on_time"])
+        print("Phone --",self.Phone["screen_on_time"],"\n")
+        print("Notification recived in a day \n")
+        print("Watsapp --",self.Watsapp["notification_recived"])
+        print("Snapchat --",self.Snapchat["notification_recived"])
+        print("Insatgram --",self.Insatgram["notification_recived"])
+        print("Youtube --",self.Youtube["notification_recived"])
+        print("Phone --",self.Phone["notification_recived"],"\n")
+        print("Apps opened in a day \n")
+        print("Watsapp --",self.Watsapp["times_opened"])
+        print("Snapchat --",self.Snapchat["times_opened"])
+        print("Insatgram --",self.Insatgram["times_opened"])
+        print("Youtube--",self.Youtube["times_opened"])
+        print("Phone --",self.Phone["times_opened"],"\n")
+    #def time_used(self):
+call = phonetracking()
+call.monday()
+call.tuesday()
+call.wednesday()
+call.thursday()
+call.friday()
+call.saturday()
+#call.time_used()
+
+                      
+
