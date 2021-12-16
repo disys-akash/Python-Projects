cefe3b3d6afccc5a327dca6b56e9e72afcec55f6
disys-akash <95430410+disys-akash@users.noreply.github.com>
2021-12-16 04:12:36 -0800

    Update transcaction.py

--- transcaction.py
+++ transcaction.py
@@ -8,7 +8,7 @@ class gpay:
             raise TypeError("Invalid name or check your name once again and try again")
         
     def check_mnumber(self):
-        self.__unumber = eval (input("Enter Mobile Number : "))
+        self.__unumber = input("Enter Mobile Number : ")
         if type(self.__unumber)==str:
             if len(self.__unumber)==10:
                 print(self.__unumber,)
@@ -20,7 +20,7 @@ class gpay:
     def check_mail(self):
         self.__umailid = input("Enter Mail ID :")
         if type(self.__umailid) == str:
-            if len(self.__umailid) <35:
+            if len(self.__umailid) >10:
                 print(self.__umailid)
                 print("Email id verified sucessfully \n")
             else:
@@ -30,7 +30,7 @@ class gpay:
     def check_upi(self):
         self.__upi = input("Enter upi id :")
         if type(self.__upi) == str:
-            if len(self.__upi) <16:
+            if len(self.__upi) < 16:
                 print(self.__upi)
                 print("Upi id verified sucessfully \n")
                 print("sucessfully verified your account \n")
@@ -42,17 +42,17 @@ class bank(gpay):
     def __init__(self):
         print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-SetUp your bank account-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
     def check_bank(self):
-        self.ubank = input("Enter your bank")
-        self.uaccno = input("Enter Bank Account number")
-        if len (self.uaccno) <=17:
+        self.__ubank = input("Enter your bank")
+        self.__uaccno = input("Enter Bank Account number")
+        if len (self.__uaccno) <=17:
             print("Succesfully Verified account number")
         else:
             raise TypeError("Check your bank account number")
     def pin_creation(self):
-        self.pin = input("Set up a 4 digit upi pin ")
-        if len(self.pin) == 4:
-            self.repin = input("re enter pin")
-            if self.pin==self.repin:
+        self.__pin = input("Set up a 4 digit upi pin ")
+        if len(self.__pin) == 4:
+            self.__repin = input("re enter pin")
+            if self.__pin==self.__repin:
                 print("Pin Setup Sucessful")
             else:
                 raise TypeError("Incorrect pin , Try again")
@@ -60,15 +60,15 @@ class bank(gpay):
             print("Re-enter 4 digit pin")
 class userdata(bank):
     def __init__(self):
-            self.alldata=[self.__uname,self.__unumber,self.__umailid,self.__upi,self.ubank,self.uaccnos,elf.pin]
-            print(self.alldata)
+            self.__alldata=[self.__uname,self.__unumber,self.__umailid,self.__upi,self.__ubank,self.__uaccnos,self.__pin]
+            print(self.__alldata)
 class payment(bank):
     def __init__(self):
         print("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-Payment-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*")
     def reciver_data(self,ifsc):
         self.__rname = input("Enter Reciver's Name : ")
         if type(self.__rname) == str:
-                self.bankname= input("Enter Reciber's Bank :")
+                self.__bankname= input("Enter Reciber's Bank :")
                 
                 self.__ifsccode = ifsc
                 print (ifsc)
@@ -91,24 +91,24 @@ class payment(bank):
             else:
                 raise ValueError("maximum number of limit crossed check and try agani")
     def e(self):
-            self.amount = input("Enter amount to be transfered : ")
-            self.pin = input("Enter your 4 digit pin to make transcaction : ")
-            if self.pin == "0075":
+            self.__amount = input("Enter amount to be transfered : ")
+            self.__pin = input("Enter your 4 digit pin to make transcaction : ")
+            if self.__pin == "0075":
                 print("Transcaction sucess \n")
                 print("you earned a new scratch card")
 
             else:
                 print("Incorect pin try again")
-user = gpay()
-user.check_mnumber()
-user.check_mail()
-user.check_upi()
-user1=bank()
-user1.check_bank()
-user1.pin_creation()
-#user2 = userdata()
-user3 = payment()
-user3.reciver_data("CIUB0000517")
-user3.transcaction()
-user3.e()
+#user = gpay()
+#user.check_mnumber()
+#user.check_mail()
+#user.check_upi()
+#user1=bank()
+#user1.check_bank()
+#user1.pin_creation()
+user2 = userdata()
+#user3 = payment()
+#user3.reciver_data("CIUB0000517")
+#user3.transcaction()
+#user3.e()
 
